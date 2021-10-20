47b7a9e51840f1bfbadfeda9dd618802142cd7a3
Jason D'Oyley <jdfd.dev@icloud.com>
2021-10-20 19:46:51 -0400

    Add script to add and minus cc

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,14 @@ let sugar = 0   // Sugar Sprinkle
 document.getElementById('credit').textContent = `Created by ${yourName}`;
 
 let gbQty = document.getElementById('qty-gb');
+let ccQty = document.getElementById('qty-cc');
+let sugarQty = document.getElementById('qty-sugar');
+let totalQty = document.getElementById('qty-total');
+
+// Recalculate total on each quantity change
+function total() {
+  return gb + cc + sugar;
+}
 
 // Event listener for Ginger Bread cookies
 
@@ -17,7 +25,42 @@ let gbQty = document.getElementById('qty-gb');
 document.getElementById('add-gb').addEventListener('click', function () {
   // Write the code to be run when the "+" button for "Ginger Bread" is clicked
   gbQty.innerText = ++gb;
+  totalQty.innerText = total();
 });
 
+// Minus Quantity
+document.getElementById('minus-gb').addEventListener('click', function () {
+  // Write the code to be run when the "+" button for "Ginger Bread" is clicked
 
-// TODO: Hook up event listeners for the rest of the buttons
+  // Check if quantity is equal to zero
+  if (parseInt(gbQty.innerText) === 0) {
+    gbQty.innerText = 0;
+  } else {
+    gbQty.innerText = --gb;
+  }
+
+  totalQty.innerText = total();
+});
+
+// Event listener for Chocolate Chip cookies
+
+// Add Quantity
+document.getElementById('add-cc').addEventListener('click', function () {
+  // Write the code to be run when the "+" button for "Chocolate Chip" is clicked
+  ccQty.innerText = ++cc;
+  totalQty.innerText = total();
+});
+
+// Minus Quantity
+document.getElementById('minus-cc').addEventListener('click', function () {
+  // Write the code to be run when the "+" button for "Chocolate Chip" is clicked
+
+  // Check if quantity is equal to zero
+  if (parseInt(ccQty.innerText) === 0) {
+    ccQty.innerText = 0;
+  } else {
+    ccQty.innerText = --cc;
+  }
+  totalQty.innerText = total();
+});
+
