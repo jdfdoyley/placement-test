4ab735e82e788ef4327b8df389609a29ab2f4a4f
Jason D'Oyley <jdfd.dev@icloud.com>
2021-10-20 19:20:18 -0400

    Add event to add gb cookies

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,23 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+const yourName = "Jason D'Oyley" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
 
-// Code to update name display 
-document.getElementById('credit').textContent = `Created by ${yourName}`
+// Code to update name display
+document.getElementById('credit').textContent = `Created by ${yourName}`;
 
-// Event listener for clicks on the "+" button for Ginger Bread cookies
-document.getElementById('add-gb').addEventListener('click', function() {
-    // HINT: You can delete this console.log after you no longer need it!
-    console.log('Ginger bread + button was clicked!')
+let gbQty = document.getElementById('qty-gb');
 
-    // TODO: Write the code to be run when the "+" button for "Ginger Bread" is clicked
-})
+// Event listener for Ginger Bread cookies
+
+// Add Quantity
+document.getElementById('add-gb').addEventListener('click', function () {
+  // Write the code to be run when the "+" button for "Ginger Bread" is clicked
+  gbQty.innerText = ++gb;
+});
+
 
 // TODO: Hook up event listeners for the rest of the buttons
