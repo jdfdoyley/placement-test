24ef7fa4edc1285c6b3b40ae6b25b157939aa86a
Jason D'Oyley <jdfd.dev@icloud.com>
2021-10-21 10:13:36 -0400

    Update js script for readability

--- assets/script.js
+++ assets/script.js
@@ -1,86 +1,70 @@
-// First, tell us your name
-const yourName = "Jason D'Oyley" // HINT: Replace this with your own name!
+const yourName = "Jason D'Oyley"
 
-// We'll use these variables to track the counts of each cookie type
 let gb = 0      // Ginger bread
 let cc = 0      // Chocolate Chip
 let sugar = 0   // Sugar Sprinkle
-
-// Code to update name display
-document.getElementById('credit').textContent = `Created by ${yourName}`;
-
 let gbQty = document.getElementById('qty-gb');
 let ccQty = document.getElementById('qty-cc');
 let sugarQty = document.getElementById('qty-sugar');
 let totalQty = document.getElementById('qty-total');
 
 // Recalculate total on each quantity change
-function total() {
-  return gb + cc + sugar;
-}
+const total = () => gb + cc + sugar;
 
-// Event listener for Ginger Bread cookies
+// Code to update name display
+document.getElementById('credit').textContent = `Created by ${yourName}`;
 
-// Add Quantity
-document.getElementById('add-gb').addEventListener('click', function () {
-  // Write the code to be run when the "+" button for "Ginger Bread" is clicked
-  gbQty.innerText = ++gb;
-  totalQty.innerText = total();
+// Add Quantity for Ginger Bread cookies
+document.getElementById('add-gb').addEventListener('click', () => {
+  gbQty.textContent = `${++gb}`;
+  totalQty.textContent = `${total()}`;
 });
 
-// Minus Quantity
-document.getElementById('minus-gb').addEventListener('click', function () {
-  // Write the code to be run when the "+" button for "Ginger Bread" is clicked
-
+// Minus Quantity for Ginger Bread cookies
+document.getElementById('minus-gb').addEventListener('click', () => {
   // Check if quantity is equal to zero
-  if (parseInt(gbQty.innerText) === 0) {
-    gbQty.innerText = 0;
+  if (parseInt(gbQty.textContent) === 0) {
+    gbQty.textContent = '0';
   } else {
-    gbQty.innerText = --gb;
+    gbQty.textContent = `${--gb}`;
   }
 
-  totalQty.innerText = total();
+  totalQty.textContent = `${total()}`;
 });
 
-// Event listener for Chocolate Chip cookies
 
-// Add Quantity
-document.getElementById('add-cc').addEventListener('click', function () {
-  // Write the code to be run when the "+" button for "Chocolate Chip" is clicked
-  ccQty.innerText = ++cc;
-  totalQty.innerText = total();
+// Add Quantity for Chocolate Chip cookies
+document.getElementById('add-cc').addEventListener('click', () => {
+  ccQty.textContent = `${++cc}`;
+  totalQty.textContent = `${total()}`;
 });
 
-// Minus Quantity
-document.getElementById('minus-cc').addEventListener('click', function () {
-  // Write the code to be run when the "+" button for "Chocolate Chip" is clicked
-
+// Minus Quantity for Chocolate Chip cookies
+document.getElementById('minus-cc').addEventListener('click', () => {
   // Check if quantity is equal to zero
-  if (parseInt(ccQty.innerText) === 0) {
-    ccQty.innerText = 0;
+  if (parseInt(ccQty.textContent) === 0) {
+    ccQty.textContent = '0';
   } else {
-    ccQty.innerText = --cc;
+    ccQty.textContent = `${--cc}`;
   }
-  totalQty.innerText = total();
+  totalQty.textContent = `${total()}`;
 });
 
 
-// Event listener for Sugar Sprinkle cookies
-
-// Add Quantity
-document.getElementById('add-sugar').addEventListener('click', function () {
-  sugarQty.innerText = ++sugar;
-  totalQty.innerText = total();
+// Add Quantity for Sugar Sprinkle cookies
+document.getElementById('add-sugar').addEventListener('click', () => {
+  sugarQty.textContent = `${++sugar}`;
+  totalQty.textContent = `${total()}`;
 });
 
-// Minus Quantity
-document.getElementById('minus-sugar').addEventListener('click', function () {
+// Minus Quantity for Sugar Sprinkle cookies
+document.getElementById('minus-sugar').addEventListener('click', () => {
   // Check if quantity is equal to zero
-  if (parseInt(sugarQty.innerText) === 0) {
-    sugarQty.innerText = 0;
+  if (parseInt(sugarQty.textContent) === 0) {
+    sugarQty.textContent = '0';
   } else {
-    sugarQty.innerText = --sugar;
+    sugarQty.textContent = `${--sugar}`;
   }
-  totalQty.innerText = total();
+  totalQty.textContent = `${total()}`;
 });
 
